81534e5280cd1a09e60a4ac3b64869d3ee1db4b4
mlkyzgt <mlkyzgt@gmail.com>
2024-07-05 15:07:59 +0300

    Initial commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,12 +3,18 @@ import { mainnet, sepolia } from 'wagmi/chains'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 
 export function getConfig() {
+  const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
+
+  if (!projectId) {
+    throw new Error('NEXT_PUBLIC_WC_PROJECT_ID is not defined.');
+  }
+
   return createConfig({
     chains: [mainnet, sepolia],
     connectors: [
       injected(),
       coinbaseWallet(),
-      walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
+      walletConnect({ projectId }), // projectId değeri tanımlı ve undefined değilse burada kullanılıyor
     ],
     storage: createStorage({
       storage: cookieStorage,
